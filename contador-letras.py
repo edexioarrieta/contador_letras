d22c695f244fd1ec7471683786ccfc1a0e7558a7
edexio <edexioarrieta@gmail.com>
2024-08-17 23:31:06 -0300

    cantidad de letras que contiene una palabra

---
+++ contador-letras.py
@@ -0,0 +1,11 @@
+# Paso 1: Solicitar al usuario que ingrese una palabra
+
+palabra_ingresada = input("Ingresa una palabra: ")
+
+# Paso 2: Contar la cantidad de letras que contiene la palabra
+
+cant_letras = len(palabra_ingresada)
+
+# Paso 3: Mostrar al usuario
+
+print("La cantidad de letras es: ", cant_letras)
